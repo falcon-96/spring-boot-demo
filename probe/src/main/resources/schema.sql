1f19c812e48337f76a5f1fe1cd1089761d5f7cbe
csd <chetan_dhanjal@yahoo.com>
2023-10-06 23:59:18 +0530

    Restructure

--- probe/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS Droids(
-    name VARCHAR(50),
-    model VARCHAR(50)
-);
-
-CREATE TABLE IF NOT EXISTS Response_Errors(
-    message VARCHAR(200),
-    datetime TIMESTAMP
-);
